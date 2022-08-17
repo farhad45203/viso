4b3dfd8426b0c8b4dce7902d67069bf435aea867
Md Farhad <80383399+farhad45203@users.noreply.github.com>
2022-08-18 03:05:41 +0600

    Viso first

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function($){
+    $(".slider").owlCarousel({
+        items: 1,
+        dots: true,
+        nav: false,
+        loop: true,
+        autoplay:false,
+        autoplayTimeout:3000,
+        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+    });
+    
+    $(".testimonial-info").owlCarousel({
+        items: 1,
+        dots: false,
+        nav: true,
+        loop: true,
+        animateOut: 'fadeOut',
+        autoplay:true,
+        autoplayTimeout:5000,
+        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+    });
+    
+    $(".agent-slider").owlCarousel({
+        items: 3,
+        slideMove:1,
+        margin:30,
+        dots: false,
+        nav: true,
+        loop: true,
+        animateOut: 'fadeOut',
+        autoplay:true,
+        autoplayTimeout:5000,
+        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        responsive:{
+        992:{
+            items:3
+        },
+        768:{
+            items:2
+        },
+        0:{
+            items:1
+        }}
+    });
+    
+});
