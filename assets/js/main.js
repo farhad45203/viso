4f1da88f9842dd19db6c21867212e9722b78cf25
farhad45203 <farhad45203@gmail.com>
2023-05-31 18:28:22 +0600

    Mobile menu updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,67 @@
-jQuery(document).ready(function($){
-    $(".slider").owlCarousel({
-        items: 1,
-        dots: true,
-        nav: false,
-        loop: true,
-        autoplay:false,
-        autoplayTimeout:3000,
-        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
-    });
-    
-    $(".testimonial-info").owlCarousel({
-        items: 1,
-        dots: false,
-        nav: true,
-        loop: true,
-        animateOut: 'fadeOut',
-        autoplay:true,
-        autoplayTimeout:5000,
-        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
-    });
-    
-    $(".agent-slider").owlCarousel({
+jQuery(document).ready(function ($) {
+  $(".slider").owlCarousel({
+    items: 1,
+    dots: true,
+    nav: false,
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    navText: [
+      "<i class='fa fa-chevron-left'></i>",
+      "<i class='fa fa-chevron-right'></i>",
+    ],
+  });
+
+  $(".testimonial-info").owlCarousel({
+    items: 1,
+    dots: false,
+    nav: true,
+    loop: true,
+    animateOut: "fadeOut",
+    autoplay: true,
+    autoplayTimeout: 5000,
+    navText: [
+      "<i class='fa fa-chevron-left'></i>",
+      "<i class='fa fa-chevron-right'></i>",
+    ],
+  });
+
+  $(".agent-slider").owlCarousel({
+    items: 3,
+    slideMove: 1,
+    margin: 30,
+    dots: false,
+    nav: true,
+    loop: true,
+    animateOut: "fadeOut",
+    autoplay: true,
+    autoplayTimeout: 5000,
+    navText: [
+      "<i class='fa fa-chevron-left'></i>",
+      "<i class='fa fa-chevron-right'></i>",
+    ],
+    responsive: {
+      992: {
         items: 3,
-        slideMove:1,
-        margin:30,
-        dots: false,
-        nav: true,
-        loop: true,
-        animateOut: 'fadeOut',
-        autoplay:true,
-        autoplayTimeout:5000,
-        navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-        responsive:{
-        992:{
-            items:3
-        },
-        768:{
-            items:2
-        },
-        0:{
-            items:1
-        }}
-    });
-    
+      },
+      768: {
+        items: 2,
+      },
+      0: {
+        items: 1,
+      },
+    },
+  });
+
+  $(".navBar").click(function () {
+    $(".mobileNav").addClass("show");
+    $(".navClose").addClass("show");
+    $(".navBar").addClass("hide");
+  });
+
+  $(".navClose, .bannerArea").click(function () {
+    $(".mobileNav").removeClass("show");
+    $(".navClose").removeClass("show");
+    $(".navBar").removeClass("hide");
+  });
 });
--- custom.js
+++ custom.js
@@ -1,21 +1,26 @@
-$(".my_btn").click(function(){
-        $("#testimonial-info").html('<div class="testimonial-info">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
-                        <div class="col-md-6">
-                            <div class="avatar">
-                                <img src="assets/img/Avatar.png">
-                            </div>
-                            <div class="rateing">
-                                <h6>Md Farhad</h6>
-                                <img src="assets/img/rateing.png">
-                            </div>
-                        </div>
-                    </div>');
-    });
-     $('a.smooth-scroll').on("click", function (e) {
-    var anchor = $(this);
-    $('html, body').stop().animate({
-        scrollTop: $(anchor.attr('href')).offset().top - 50
-    }, 1000);
-    e.preventDefault();
-	});
+$(".my_btn").click(function () {
+  $("#testimonial-info").html(`<div class="testimonial-info">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. </p>
+        <div class="col-md-6">
+            <div class="avatar">
+                <img src="assets/img/Avatar.png">
+            </div>
+            <div class="rateing">
+                <h6>Md Farhad</h6>
+                <img src="assets/img/rateing.png">
+            </div>
+        </div>
+    </div>`);
+});
+$("a.smooth-scroll").on("click", function (e) {
+  var anchor = $(this);
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $(anchor.attr("href")).offset().top - 50,
+      },
+      1000
+    );
+  e.preventDefault();
+});
